a37a35009db6b3b60feac588862a1344be8e086a
6Waifu9 <Carlos@speldekamp.nl>
2022-12-06 16:09:02 +0100

    Hondje mag rennen en idle staan!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,41 +5,65 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    private Animator anim;
+
     public float moveSpeed = 5f;
     public Transform movePoint;
 
+
     public LayerMask whatStopsMovement;
-    void start() 
+    void start()
     {
         movePoint.parent = null;
+    }
+
+    private void Awake() 
+    {
+        anim = GetComponent<Animator>();
     }
 
 
 
     void Update()
     {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, movePoint.position) <= .05f) 
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            if (Mathf.Abs(horizontalInput) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(horizontalInput, 0f, 0f), .2f, whatStopsMovement))
                 {
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                    movePoint.position += new Vector3(horizontalInput, 0f, 0f);
                 }
-                
-            }else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+
+            } else if (Mathf.Abs(verticalInput) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f,Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopsMovement))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, verticalInput, 0f), .2f, whatStopsMovement))
                 {
-                    movePoint.position += new Vector3(0f,Input.GetAxisRaw("Vertical"),0f);
+                    movePoint.position += new Vector3(0f, verticalInput, 0f);
                 }
 
             }
         }
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = new Vector3(-2.5f, 2.5f, 2f);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(2.5f,2.5f,2f);
+        }
+
+        anim.SetBool("run", horizontalInput != 0);
 
     }
+
+    
+
 
 
   
